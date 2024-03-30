f5f26a6ed6c61d01e475c745f369ed54fd26ca15
Max <sawnmax@mail.ru>
2024-03-30 18:28:59 +0400

    Initial commit

---
+++ SnakeGame/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static async Task Main(string[] args)
+    {
+        Init();
+        Task conductSnakeTask = Task.Run(() => GuideSnake());
+        Task executeGameProcessTask = Task.Run(() => ExecuteGameProcess());
+
+        Task.WaitAll(conductSnakeTask, executeGameProcessTask);
+    }
+
+    private static void Init()
+    {
+        Console.WriteLine("Welcome to Snake game, Press Any key to start");
+        var key = Console.ReadKey(true);
+    }
+
+    private static Task GuideSnake()
+    {
+        return Task.CompletedTask;
+    }
+    private static Task ExecuteGameProcess()
+    {
+        return Task.CompletedTask;
+    }
+
+
+
+}
