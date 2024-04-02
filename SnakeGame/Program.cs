b5faabff286c2d9a39756a965451fae30751e423
Max <sawnmax@mail.ru>
2024-04-02 20:55:44 +0400

    Пытаюсь втянуть все изменения

--- SnakeGame/Program.cs
+++ SnakeGame/Program.cs
@@ -1,5 +1,10 @@
-﻿class Program
+﻿using SnakeGame;
+
+class Program
 {
+    const int SizeX= 20;
+    const int SizeY = 50;
+
     static async Task Main(string[] args)
     {
 
@@ -13,19 +18,44 @@
 
     private static void Init()
     {
+        Snake snake = new Snake();
         Console.WriteLine("Welcome to Snake game, Press Any key to start");
         var key = Console.ReadKey(true);
     }
 
     private static Task GuideSnake()
     {
+
+
+
         return Task.CompletedTask;
     }
     private static Task ExecuteGameProcess()
     {
+        while (Snake.IsAlive(true))
+        {
+            
+
+            DoAct();
+            Thread.Sleep(1000);
+        }
         return Task.CompletedTask;
     }
 
+    private static void DoAct()
+    {
+        int score = 0;
+
+        while (Snake.IsAlive(true))
+        {
+            Frame.Clear();
+            Frame.Display();
+            Console.WriteLine(score);
+            Snake.Move();
+
+        }
+        
+    }
 
 
 }
