348cefb11378e8290d5b3c391128b9d7cc1f6d3b
Ancient <atlantis159@mail.ru>
2024-04-02 22:41:39 +0500

    GuideSnake

--- SnakeGame/Program.cs
+++ SnakeGame/Program.cs
@@ -1,14 +1,15 @@
-﻿class Program
+﻿using SnakeGame;
+
+class Program
 {
-    const int SizeX= 20;
+    const int SizeX = 20;
     const int SizeY = 50;
 
     static async Task Main(string[] args)
     {
-
         //Team1 starts
         Init();
-        Task conductSnakeTask = Task.Run(() => GuideSnake());
+        Task conductSnakeTask = Task.Run(() => GuideSnake(new Snake()));
         Task executeGameProcessTask = Task.Run(() => ExecuteGameProcess());
 
         Task.WaitAll(conductSnakeTask, executeGameProcessTask);
@@ -20,13 +21,40 @@
         var key = Console.ReadKey(true);
     }
 
-    private static Task GuideSnake()
+    private static Task GuideSnake(Snake snake)
     {
+        while (snake.IsAlive)
+        {
+            Direction nextDirection;
+            
+            switch (Console.ReadKey(true).Key)
+            {
+                case ConsoleKey.W:
+                case ConsoleKey.UpArrow:
+                    nextDirection = Direction.UP;
+                    break;
+                case ConsoleKey.S:
+                case ConsoleKey.DownArrow:
+                    nextDirection = Direction.DOWN;
+                    break;
+                case ConsoleKey.A:
+                case ConsoleKey.LeftArrow:
+                    nextDirection = Direction.LEFT;
+                    break;
+                case ConsoleKey.D:
+                case ConsoleKey.RightArrow:
+                    nextDirection = Direction.RIGHT;
+                    break;
+                default:
+                    continue;
+            }
 
-
-
+            snake.Direction = nextDirection;
+        }
+        
         return Task.CompletedTask;
     }
+
     private static Task ExecuteGameProcess()
     {
         while (true)
@@ -34,13 +62,11 @@
             DoAct();
             Thread.Sleep(1000);
         }
+
         return Task.CompletedTask;
     }
 
     private static void DoAct()
     {
-
     }
-
-
 }
