c3fb88445a0afc93a52485330a61542944846e4a
magus.mgl@yandex.ru <magus_mgl@yandex.ru>
2024-04-03 01:24:47 +0500

    конструктор и метод Move для Snake

--- SnakeGame/Snake.cs
+++ SnakeGame/Snake.cs
@@ -9,7 +9,7 @@ namespace SnakeGame
     public class Snake
     {
         private Pixel _head;
-        public Queue<Pixel> Pixels {  get; set; }
+        public Queue<Pixel> Pixels { get; set; }
 
         public Direction Direction { get; set; }
 
@@ -17,9 +17,41 @@ namespace SnakeGame
 
         public bool IsAlive { get; } = true;
 
-        public void Move()
+        public void Move(Direction directionToGo)
         {
-
+            Pixels.Dequeue();
+            
+            switch (directionToGo)
+            {
+                case Direction.UP:
+                    if (Direction != Direction.DOWN)
+                    {
+                        _head = new Pixel(_head.X, _head.Y-1, Image.Snake);
+                        Pixels.Enqueue(_head);
+                    }
+                    break;
+                case Direction.DOWN:
+                    if (Direction != Direction.UP)
+                    {
+                        _head = new Pixel(_head.X, _head.Y + 1, Image.Snake);
+                        Pixels.Enqueue(_head);
+                    }
+                    break;
+                case Direction.LEFT:
+                    if (Direction != Direction.RIGHT)
+                    {
+                        _head = new Pixel(_head.X-1, _head.Y, Image.Snake);
+                        Pixels.Enqueue(_head);
+                    }
+                    break;
+                case Direction.RIGHT:
+                    if (Direction != Direction.LEFT)
+                    {
+                        _head = new Pixel(_head.X+1, _head.Y, Image.Snake);
+                        Pixels.Enqueue(_head);
+                    }
+                    break;
+            }
         }
 
         public void TryEatDot()
@@ -28,17 +60,22 @@ namespace SnakeGame
         }
         public Snake(int sizeX, int sizeY)
         {
+            Pixels = new Queue<Pixel>();
+
             int xHead = sizeX / 2;
             int yHead = sizeY / 2;
             _head = new Pixel(xHead, yHead, Image.Snake);
-            Pixels.Enqueue(_head);
-            for (int i = 0; i < 2; i++)
-            {
-                Pixel pixel =  new  Pixel(xHead-1, yHead, Image.Snake); 
-            }
-            Direction = Direction.LEFT;
 
+            for (int i = 2; i > 0; i--)
+            {
+                var pixel = new Pixel(xHead + i, yHead, Image.Snake);
+                Pixels.Enqueue(pixel);
+            }
+
+            Pixels.Enqueue(_head);
+            Direction = Direction.LEFT;
         }
+
 
         private void HealthCheck()
         {
