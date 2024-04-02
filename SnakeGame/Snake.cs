e9068a83ee3340e418407e6b45798e0d575a14c0
Anton-Sleptsov <arslepcov@kait20.ru>
2024-04-02 22:07:45 +0300

    Add method TryEatDot

--- SnakeGame/Snake.cs
+++ SnakeGame/Snake.cs
@@ -22,9 +22,41 @@ namespace SnakeGame
 
         }
 
-        public void TryEatDot()
+        public void TryEatDot(Dot dot)
         {
+            switch (Direction)
+            {
+                case Direction.DOWN:
+                    if(_head.Y - 1 == dot.Pixel.Y && _head.X == dot.Pixel.X)
+                    {
+                        EatingDot(dot);
+                    }
+                    break;
+                case Direction.UP:
+                    if (_head.Y + 1 == dot.Pixel.Y && _head.X == dot.Pixel.X)
+                    {
+                        EatingDot(dot);
+                    }
+                    break;
+                case Direction.LEFT:
+                    if (_head.X - 1 == dot.Pixel.X && _head.Y == dot.Pixel.Y)
+                    {
+                        EatingDot(dot);
+                    }
+                    break;
+                case Direction.RIGHT:
+                    if (_head.X + 1 == dot.Pixel.X && _head.Y == dot.Pixel.Y)
+                    {
+                        EatingDot(dot);
+                    }
+                    break;
+            }
+        }
 
+        private void EatingDot(Dot dot)
+        {
+            Pixels.Enqueue(dot.Pixel);
+            dot.Generate();
         }
 
         private void HealthCheck()
