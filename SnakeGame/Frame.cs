582e5dba04c231b8c36aa3eb1d969047f15fefa9
TatianaNai <tatyana_tat333@mail.ru>
2024-04-03 19:23:35 +0300

    Frame updated

--- SnakeGame/Frame.cs
+++ SnakeGame/Frame.cs
@@ -8,40 +8,40 @@ namespace SnakeGame
 {
     public class Frame
     {
-        private char[,] _values;
+        private Image[,] _values;
 
         private int _sizeX;
         private int _sizeY;
 
-        public Frame(int _sizeX, int _sizeY) 
+        public Frame(int _sizeY, int _sizeX) 
         {
-            _values = new char[_sizeX, _sizeY];
-            int X = 0;
-            for (int j = 0; j < _sizeY; j++)
-            {
-                _values[X, j] = '-';
-            }
-            X = _sizeX - 1;
-            for (int j = 0; j < _sizeY; j++)
-            {
-                _values[X, j] = '-';
-            }
+            _values = new Image[_sizeY, _sizeX];
             int Y = 0;
-            for (int i = 1; i < _sizeX - 1; i++)
+            for (int j = 0; j < _sizeX; j++)
             {
-                _values[i, Y] = '|';
-            } 
+                _values[Y, j] = Image.HorizontalBorder;
+            }
             Y = _sizeY - 1;
-            for (int i = 1; i < _sizeX - 1; i++)
+            for (int j = 0; j < _sizeX; j++)
             {
-                _values[i, Y] = '|';
+                _values[Y, j] = Image.HorizontalBorder;
+            }
+            int X = 0;
+            for (int i = 1; i < _sizeY - 1; i++)
+            {
+                _values[i, X] = Image.VerticalBorder;
+            } 
+            X = _sizeX - 1;
+            for (int i = 1; i < _sizeY - 1; i++)
+            {
+                _values[i, X] = Image.VerticalBorder;
             }
 
             for (int i = 1; i < _values.GetLength(0) - 1; i++)
             {
                 for (int j = 1; j < _values.GetLength(1) - 1; j++)
                 {
-                    _values[i, j] = ' ';
+                    _values[i, j] = Image.Empty;
                 }
             }
         }
@@ -50,13 +50,13 @@ namespace SnakeGame
         {
             foreach (var item in snake.Pixels)
             {
-                _values[item.X, item.Y] = '*';
+                _values[item.Y, item.X] = Image.Snake;
             }
         }
 
         public void SetDot(Dot dot)
         {
-            _values[dot.Pixel.X, dot.Pixel.Y] = '*';
+            _values[dot.Pixel.Y, dot.Pixel.X] = Image.Dot;
         }
 
         public void Clear()
@@ -65,7 +65,7 @@ namespace SnakeGame
             {
                 for (int j = 1; j < _values.GetLength(1) - 1; j++)
                 {
-                    _values[i,j] = ' ';
+                    _values[i,j] = Image.Empty;
                 }
             }
         }
@@ -77,7 +77,22 @@ namespace SnakeGame
             {
                 for (int j = 0; j < _values.GetLength(1); j++)
                 {
-                    Console.Write(_values[i,j]);
+                    switch (_values[i, j])
+                    {
+                        case Image.Empty:
+                            Console.Write(" ");
+                            break;
+                        case Image.Snake:
+                        case Image.Dot:
+                            Console.Write("*");
+                            break;
+                        case Image.VerticalBorder:
+                            Console.Write("|");
+                            break;
+                        case Image.HorizontalBorder:
+                            Console.Write("-");
+                            break;
+                    }
                 }
                 Console.WriteLine();
             }
