88a17ae432104fb9d6da39b2507cfc17854d33a8
Georg <gash20161@mail.ru>
2024-04-03 21:13:07 +0300

    guideSnake method included

--- SnakeGame/Program.cs
+++ SnakeGame/Program.cs
@@ -1,5 +1,5 @@
 ﻿using SnakeGame;
-
+using System.Xml.Linq;
 class Program
 {
     const int SizeX= 20;
@@ -8,10 +8,9 @@ class Program
 
     static async Task Main(string[] args)
     {
-       
         //team2
         Init();
-        Task conductSnakeTask = Task.Run(() => GuideSnake());
+        Task conductSnakeTask = Task.Run(() => GuideSnake( snake));
         Task executeGameProcessTask = Task.Run(() => ExecuteGameProcess());
 
         Task.WaitAll(conductSnakeTask, executeGameProcessTask);
@@ -23,11 +22,33 @@ class Program
         var key = Console.ReadKey(true);
     }
 
-    private static Task GuideSnake()
+    private static Task GuideSnake(Snake snake)
     {
-     
-
+        while (snake.IsAlive)
+        {
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            switch(keyInfo.Key)
+            {
+                case ConsoleKey.LeftArrow:
+                    snake.Direction = Direction.LEFT;
+                    break;
+                case ConsoleKey.RightArrow:
+                    snake.Direction = Direction.RIGHT;
+                    break ;
+                case ConsoleKey.UpArrow:
+                    snake.Direction = Direction.UP;
+                    break ;
+                case ConsoleKey.DownArrow:
+                    snake.Direction= Direction.DOWN;
+                    break ;
+                default:
+                    break;
+            }
+            DoAct();
+            Thread.Sleep(1000);
+        }
         return Task.CompletedTask;
+
     }
     private static Task ExecuteGameProcess()
     {
