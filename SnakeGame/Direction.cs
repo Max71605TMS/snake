1cce561bee5082ea456ced724b08312a1a6013a3
Илья <@DESKTOP-8ICT6FH>
2024-04-04 13:56:11 +0300

    Все удалил

--- SnakeGame/Direction.cs
+++ SnakeGame/Direction.cs
@@ -1,16 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SnakeGame
-{
-    public enum Direction
-    {
-        UP,
-        DOWN, 
-        LEFT, 
-        RIGHT
-    }
-}
+﻿
--- SnakeGame/Image.cs
+++ SnakeGame/Image.cs
@@ -1,17 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SnakeGame
-{
-    public enum Image
-    {
-        Empty,
-        Snake,
-        Dot,
-        VerticalBorder,
-        HorizontalBorder,
-    }
-}
+﻿
